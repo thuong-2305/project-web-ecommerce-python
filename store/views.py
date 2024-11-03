ca53acd773718cb269f61feafe685f4e96055821
T2315 <nguyenthanhthuong.23052004@gmail.com>
2024-11-04 06:32:23 +0700

    Handle category

--- store/views.py
+++ store/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm
-from django import forms
+
+def category(request, foo):
+    try:
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products':products, 'category':category})
+    except:
+        messages.success("That category don' exits...")
+        return redirect('home')
 
 def product(request, pk):
     product = Product.objects.get(id=pk)
@@ -15,7 +21,6 @@ def product(request, pk):
     return render(request, 'product.html', 
         {'product':product, 'description':description, 'price':price, 'sale_price':sale_price}
     )
-
 
 def home(request):
     products = Product.objects.all()
