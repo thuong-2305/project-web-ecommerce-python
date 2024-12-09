2226a2152f338cfca8b468bdb3540a6cd6616d13
T2315 <nguyenthanhthuong.23052004@gmail.com>
2024-12-09 19:19:57 +0700

    Them mo rong cho profile voi dia chi va sdt

--- store/models.py
+++ store/models.py
@@ -3,15 +3,27 @@ from datetime import datetime
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
+# ------------------------ User ---------------------------
+# create customer profile
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_modified = models.DateTimeField(User, auto_now=True)
+    phone = models.CharField(max_length=20, blank=True)
+    address = models.CharField(max_length=200, blank=True)
+    
+    def __str__(self):
+        return self.user.username
+    
+# Create a profile user by default when user sign up
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
 
-# # create customer profile
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     date_modified = models.DateTimeField(User, auto_now=True)
-#     phone = models.CharField(max_length=20, blank=True)
-#     address = models.CharField(max_length=200, blank=True)
-#     pass
+# Automate the profile thing
+post_save.connect(create_profile, sender=User)
 
+# ------------------------ Product -------------------------
 #Categories of products
 class Category(models.Model):
     name = models.CharField(max_length=50)
