91524350df8f4497ce6e0768ef42026d5068417b
T2315 <nguyenthanhthuong.23052004@gmail.com>
2024-12-11 04:00:20 +0700

    Xu ly them san pham vao wishlist

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render, redirect
-from cart.cart import Cart
+from django.shortcuts import render, redirect, get_object_or_404
+from store.views import product
+from .wishlist import Wishlist
 from store.models import Product
 from django.http import JsonResponse
 
 def wishlist(request):
-    return render(request, 'wishlist.html', {})
+    wishlist= Wishlist(request)
+    wishlist_products = wishlist.get_prods()
+    price = wishlist.get_price()
+    return render(request, 'wishlist.html', {'wishlist_products':wishlist_products, 'price':price})
+
+def wishlist_add(request):
+    wishlist = Wishlist(request)
+    if request.method == 'POST':
+        product_id = str(request.POST.get('product_id'))
+        product_price = str(request.POST.get('product_price'))
+
+        product = get_object_or_404(Product, id=product_id)
+        msg = wishlist.add_wish(product, product_price)
+        
+        response = JsonResponse({'msg': msg})
+        return response
---
+++ wishlist/wishlist.py
@@ -0,0 +1,29 @@
+from store.models import Product
+class Wishlist():
+    def __init__(self, request):
+        self.session = request.session
+        wishlist = self.session.get('wishlist_session_key')
+        if 'wishlist_session_key' not in request.session:
+            wishlist = self.session['wishlist_session_key'] = {}
+        self.wishlist = wishlist
+
+    def add_wish(self, product, price):
+        product_id = str(product.id)
+        product_price = str(price)
+        msg = ""
+        if product_id in self.wishlist:
+            msg = "Sản phẩm đã có trong giả hàng"
+        else:
+            self.wishlist[product_id] = str(product_price)
+            msg = "Thêm vào giỏ hàng thành công"
+        self.session.modified = True
+        return msg
+    
+    def get_prods(self):
+        product_ids = self.wishlist.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        return products
+    
+    def get_price(self):
+        price = self.wishlist
+        return price
