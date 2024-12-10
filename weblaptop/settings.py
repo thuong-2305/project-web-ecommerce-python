7f296f3558fedcc5b2f686fa817eab9d23c2c4ca
T2315 <nguyenthanhthuong.23052004@gmail.com>
2024-12-10 10:42:21 +0700

    Them wishlist vao project

--- weblaptop/settings.py
+++ weblaptop/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'store',
     'cart',
+    'wishlist',
 ]
 
 MIDDLEWARE = [
---
+++ wishlist/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+from cart.cart import Cart
+from store.models import Product
+from django.http import JsonResponse
+
+def wishlist(request):
+    return render(request, 'wishlist.html', {})
